5993b6b35da10da9d5a15b850b41cbb799fad4a5
Ankit <ankitspatel1145@gmail.com>
2020-01-10 10:28:16 -0800

    update nav bar

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -5,30 +5,31 @@ import GreetingContainer from './greeting/greeting_container';
 class NavBar extends Component {
   render() {
     return (
-      <nav className="nav-bar">
-        <div className="logo-div">
-          <Link to="/" className="header-link">
+      <div className='nav-bar'>
+        <div className="nav-image">
+          <Link to="/" >
             <img src="/assets/logo.png" width="100%" />
           </Link>
         </div>
-        <div className="nav-links">
-
-        <ul className="link-list">
-          <li>
-            Events
-          </li>
-          <li>
-            Hosting
-          </li>
-          <li>
-            About
-          </li>
-        <GreetingContainer />
-        </ul>
-        </div>
-
-      </nav>
- 
+        
+        <nav className="nav-list">
+          <ul className="link-list-ul">
+            <li>
+              Events
+            </li>
+            <li>
+              Hosting
+            </li>
+            <li>
+              About
+            </li>
+            <li>
+              <GreetingContainer />
+            </li>
+          </ul>
+        </nav>
+     
+      </div>
  );
   }
 }
