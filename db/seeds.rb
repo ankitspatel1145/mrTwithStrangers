a5b009b99d1f31e638186a73b2c0cce7de32b405
kit <ankitspatel1145@gmail.com>
2020-01-13 09:24:33 -0800

    test ssh

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+# user1 = User.create(email: "demo@twithstrangers.com", password:"password", first_name:"Demo User", city:"New York")
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user1 = User.create(email: "demo@twithstrangers.com", password:"password", first_name:"Demo User", city:"New York")
