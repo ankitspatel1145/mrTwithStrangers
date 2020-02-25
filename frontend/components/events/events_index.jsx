1dc74e934de4d28c069527453ffa44142cf4a0a8
kit <ankitspatel1145@gmail.com>
2020-02-25 11:31:49 -0800

    Clean up events index file

--- frontend/components/events/events_index.jsx
+++ frontend/components/events/events_index.jsx
@@ -7,21 +7,19 @@ class EventsIndex extends React.Component{
   constructor(props) {
     super(props)
     this.state = {}
-    // this.testq = this.testq.bind(this)
   }
 
   componentDidMount(){
     this.props.getAllEvents();
   }
 
-  // testq(){
-  //   console.log("test")
-  // }
+ 
 
   render() {
-    let test = document.getElementById('cit')
+    let LA = document.getElementById('LA')
+    let NY = document.getElementById('NY')
+    let SF = document.getElementById('SF')
     let now = new Date()    
-    console.log(now)
     let events = this.props.events
     let la = events.filter(event => {
       let eventDate = new Date(event.date)
@@ -41,19 +39,18 @@ class EventsIndex extends React.Component{
         return event
       }
     }) 
-    // debugger
     return(
       
       <div className='eventsindex'>
 
         <div className='citybtn'>
-          {/* <input type="button" onClick="document.getElementById('test').scrollIntoView();" /> */}
-          {/* <button onclick=" document.getElementById(`test`).scrollIntoView();">Scroll</button> */}
-          <button onClick={() => (test.scrollIntoView())} >test</button>
+          <button onClick={() => (NY.scrollIntoView())} >New York</button>
+          <button onClick={() => (SF.scrollIntoView())} >San Francisco</button>
+          <button onClick={() => (LA.scrollIntoView())} >Los Angeles</button>
 
         </div>
 
-        <div className='city'>
+        <div id="SF" className='city'>
           <div className='cityname'>
             <h1>Events in San Francisco:</h1>
           </div>
@@ -69,7 +66,7 @@ class EventsIndex extends React.Component{
                 </ul>
           </div>
         </div>
-        <div className='city'>
+        <div id="LA" className='city'>
           <div className='cityname'>
             <h1>Events in Los Angeles:</h1>
           </div>
@@ -85,7 +82,7 @@ class EventsIndex extends React.Component{
                 </ul>
           </div>
         </div>
-        <div id="cit" className='city'>
+        <div id="NY" className='city'>
           <div className='cityname'>
             <h1>Events in New York:</h1>
           </div>
@@ -101,9 +98,7 @@ class EventsIndex extends React.Component{
                 </ul>
           </div>
         </div>
-          <div id="test">
-          <h1 >hw</h1>
-          </div>
+          
       </div>
     );
   }
