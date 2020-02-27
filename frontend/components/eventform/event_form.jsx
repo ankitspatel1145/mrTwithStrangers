be5e4ae59816b27f8207c3c2a6158ad2e66021be
kit <ankitspatel1145@gmail.com>
2020-02-26 16:42:16 -0800

    Make date picker

--- frontend/components/eventform/event_form.jsx
+++ frontend/components/eventform/event_form.jsx
@@ -11,9 +11,12 @@ class EventForm extends React.Component {
       "state": "",
       "address": "",
       "time": "",
-      "spots": 4,
+      "spots": "",
       "description": ""
     }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.update = this.update.bind(this)
   }
 
 
@@ -23,10 +26,76 @@ class EventForm extends React.Component {
     });
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    // let formdata = new FormData()
+    let event = this.state
+    this.props.createEvent(event)
+    // this.props.history.push('/events')
+  }
+
   render() {
+    var dateObj = new Date();
+    var month = dateObj.getUTCMonth() + 1; //months from 1-12
+    var day = dateObj.getUTCDate();
+    var year = dateObj.getUTCFullYear();
+    if (month < 10){
+      month = "0" + month      
+    }
+    if (day < 10) {
+      day = "0" + day
+    }
+
+
+    let today = year + "-" + month + "-" + day;
     console.log(this.props)
+    console.log(today)
     return (
-      <h1>this is a form</h1>
+      <div className="form-container">
+
+        <form onSubmit={this.handleSubmit} className="login-form">
+          <div className="header-text">
+            <h1>Host an Event!</h1>
+          </div>
+
+          <div className="errors">
+            {/* {this.renderErrors()} */}
+          </div>
+
+          <div className="containter-form" >
+
+            <input type="date"
+              value={this.state.date}
+              min={today}
+              onChange={this.update('date')}
+              className="login-input"
+            />
+   
+            <input type="address"
+              placeholder="Address"
+              value={this.state.address}
+              onChange={this.update('address')}
+              className="login-input"
+            />
+            <input type="spots"
+              placeholder="Number of open seats"
+              value={this.state.spots}
+              onChange={this.update('spots')}
+              className="login-input"
+            />
+            <input type="description"
+              placeholder="Description"
+              value={this.state.description}
+              onChange={this.update('description')}
+              className="login-input"
+            />
+
+            <button className="session-submit" type="submit" value={this.props.formType}>Create Event</button>
+
+          </div>
+
+        </form>
+      </div>
     )
   }
 
